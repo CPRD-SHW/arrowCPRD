531a7ab530a50ec1c03d4818c21aae419b6f77ce
Andy Baxter <andrewbaxter439@gmail.com>
2026-06-25 10:01:25 +0100

    added date checking

--- tests/testthat/test-data_reading.R
+++ tests/testthat/test-data_reading.R
@@ -106,6 +106,7 @@ test_that("reading all zips in a folder to parquet works", {
     tibble::as_tibble()
 
   expect_no_error(pq_in)
+  expect_s3_class(pq_in$regstartdate, "Date")
 
   on.exit(unlink(temp_pq))
 })
